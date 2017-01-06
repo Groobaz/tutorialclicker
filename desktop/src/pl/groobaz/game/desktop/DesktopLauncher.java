cac021b50595f1c407c73f8cea3d5c6fe9c23654
Groobaz <michalrudzki1986@gmail.com>
2017-01-06 20:51:17 +0100

    config desktop luncher

--- desktop/src/pl/groobaz/game/desktop/DesktopLauncher.java
+++ desktop/src/pl/groobaz/game/desktop/DesktopLauncher.java
@@ -2,11 +2,19 @@ package pl.groobaz.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sun.xml.internal.ws.assembler.dev.TubelineAssemblyContextUpdater;
+
 import pl.groobaz.game.JavaDevMattClikerGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = JavaDevMattClikerGame.GAME_NAME;
+		config.width = JavaDevMattClikerGame.WIDTH;
+		config.height = JavaDevMattClikerGame.HEIGHT;
+		config.resizable = false;
+		
 		new LwjglApplication(new JavaDevMattClikerGame(), config);
 	}
 }
