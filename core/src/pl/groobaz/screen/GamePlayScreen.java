ce045f6c02bf0e77e7ad369779b7a3fe68d8d073
Groobaz <michalrudzki1986@gmail.com>
2017-01-07 18:56:42 +0100

    added gameScreen after splash screen

--- core/src/pl/groobaz/screen/GamePlayScreen.java
+++ core/src/pl/groobaz/screen/GamePlayScreen.java
@@ -7,6 +7,7 @@ public class GamePlayScreen extends AbstractScreen {
 	public GamePlayScreen(JavaDevMattClikerGame game) {
 		super(game);
 		init();
+		
 	}
 
 	private void init() {
--- core/src/pl/groobaz/screen/SplashScreen.java
+++ core/src/pl/groobaz/screen/SplashScreen.java
@@ -1,6 +1,8 @@
 package pl.groobaz.screen;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 
 import pl.groobaz.game.JavaDevMattClikerGame;
 
@@ -8,9 +10,16 @@ public class SplashScreen extends AbstractScreen{
 
 	private Texture splashImg;
 	
-	public SplashScreen(JavaDevMattClikerGame game) {
+	public SplashScreen(final JavaDevMattClikerGame game) {
 		super(game);
 		init();
+		Timer.schedule(new Task() {
+			
+			@Override
+			public void run() {
+				game.setScreen(new GamePlayScreen(game));
+			}
+		}, 1);
 	}
 
 	private void init() {
