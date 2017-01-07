cd629625a91c17afa761974ed42518ab063dd1b3
Groobaz <michalrudzki1986@gmail.com>
2017-01-07 18:40:41 +0100

    dummy game play screen

---
+++ core/src/pl/groobaz/screen/GamePlayScreen.java
@@ -0,0 +1,16 @@
+package pl.groobaz.screen;
+
+import pl.groobaz.game.JavaDevMattClikerGame;
+
+public class GamePlayScreen extends AbstractScreen {
+
+	public GamePlayScreen(JavaDevMattClikerGame game) {
+		super(game);
+		init();
+	}
+
+	private void init() {
+		
+	}
+	
+}
