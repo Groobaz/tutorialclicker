01313ac342df64dd6b2f4dee0317590f4c8c750c
Groobaz <michalrudzki1986@gmail.com>
2017-01-07 19:31:04 +0100

    display Player Image

--- core/src/pl/groobaz/screen/GamePlayScreen.java
+++ core/src/pl/groobaz/screen/GamePlayScreen.java
@@ -1,9 +1,12 @@
 package pl.groobaz.screen;
 
+import pl.groobaz.entities.Player;
 import pl.groobaz.game.JavaDevMattClikerGame;
 
 public class GamePlayScreen extends AbstractScreen {
 
+	private Player player; 
+	
 	public GamePlayScreen(JavaDevMattClikerGame game) {
 		super(game);
 		init();
@@ -11,7 +14,27 @@ public class GamePlayScreen extends AbstractScreen {
 	}
 
 	private void init() {
+		initPlayer();
+	}
+
+	private void initPlayer() {
+		 player = new Player();
+		 stage.addActor(player);
+	}
+	
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+		update();
 		
+		spriteBatch.begin();
+		stage.draw();
+		
+		spriteBatch.end();
+	}
+
+	private void update() {
+		stage.act();
 	}
 	
 }
