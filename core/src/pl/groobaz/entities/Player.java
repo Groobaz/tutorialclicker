f422c31677d5d9b06922df3ccf55d58bd3b02264
Groobaz <michalrudzki1986@gmail.com>
2017-01-07 20:23:16 +0100

    magic numbers Refactor in react Onclick Method

--- core/src/pl/groobaz/entities/Player.java
+++ core/src/pl/groobaz/entities/Player.java
@@ -27,16 +27,20 @@ public class Player extends Image {
 	public void reactOnClick() {
 		//Action testAction = Actions.moveBy(10, 10);
 		//this.addAction(testAction);
-		int moveAmmount = MathUtils.random(-130, 130);
+		int xMoveAmmount = MathUtils.random(-130, 130);
+		int yMoveAmmount = 10;
+		float moveActionTime = 0.3f;
 		Action moveAction = Actions.sequence(
-				Actions.moveBy(moveAmmount, 10, 0.3f, Interpolation.circleOut),
-				Actions.moveBy(-moveAmmount, -10, 0.3f, Interpolation.circle)
+				Actions.moveBy(xMoveAmmount, yMoveAmmount, moveActionTime, Interpolation.circleOut),
+				Actions.moveBy(-xMoveAmmount, -yMoveAmmount, moveActionTime, Interpolation.circle)
 		);
 		
-		int growAmmount = MathUtils.random(-30, 100);
+		int xGrowAmmount = MathUtils.random(-30, 100);
+		int yGrowAmmount = 20;
+		float growActionTime = 0.2f;
 		Action growAction = Actions.sequence(
-				Actions.sizeBy(growAmmount, 20, 0.2f, Interpolation.circleOut),
-				Actions.sizeBy(-growAmmount, -20, 0.2f, Interpolation.circle)
+				Actions.sizeBy(xGrowAmmount, yGrowAmmount, growActionTime, Interpolation.circleOut),
+				Actions.sizeBy(-xGrowAmmount, -yGrowAmmount, growActionTime, Interpolation.circle)
 		);
 		
 		this.addAction(moveAction);
